c49bd33b2078f6721466f0b0519491c1b5a6a948
steviec1 <13-13282@saeinstitute.edu>
2022-05-29 23:41:22 +0200

    yes ok
    without comments

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 
-//STANDARD STUFF HERE COPY/PASTE
+//IMPORT SOME NODE / EXPRESS MODULES
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// for error pages:  routing, controller
+// for error pages
 const errorController = require('./controllers/error');
 
 const app = express();
 const fileUpload = require("express-fileupload");
-
 
 const shopRoutes = require('./routes/shop');
 const adminRoutes = require('./routes/admin');
@@ -18,17 +17,16 @@ const userRoutes = require('./routes/user');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 
+/* SET ROUTERS FOR PATHS (e.g. router for all sites under mysite.com/admin or /shop  */
 app.use('/', shopRoutes);
 app.use('/admin', adminRoutes);
 app.use('/user', userRoutes);
 
-
+/* set controller method here  */
 app.use(errorController.get404);
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -5,15 +5,11 @@ const router = express.Router();
 
 const adminController = require('../controllers/admin');
 
- // router.get('/add-product', adminController.addProductPage);
-
  router.use('/add-product', adminController.addProductPage);
  router.post('/productsave', adminController.addProductSave);
-// router.get('/edit-product/:productId', adminController.getEditProduct);
-router.get('/edit-product/:productId?', adminController.getEditProduct);
-//use question mark here to make the id optional
-router.post('/productsave',  adminController.postEditProduct);
-// router.post('/add-product', adminController.addProductSave);
+
+router.get('/edit-product/:productId?', adminController.getEditProduct); //use question mark here to make the id optional
+//router.post('/productsave',  adminController.postEditProduct);
 
 module.exports = router;
 
