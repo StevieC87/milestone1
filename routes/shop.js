4c81078703e24124b5279bf129dcb5c71b18e551
StevieC87 <13-13282@saeinstitute.edu>
2022-06-16 18:09:52 +0200

    buttonsetc

--- routes/shop.js
+++ routes/shop.js
@@ -19,6 +19,81 @@ router.get('/getwordswithcenterletter', BeeController.centerlettermatch)
 router.post('/newgamepost?', BeeController.addNewGame);
 router.get('/play/:gameId?', BeeController.playGame);
 
+
+//HERE FOR LOCAL TEST
+
+let word = 'basketball';
+let centreletter = 'k';
+let remaininglettersarray = ['b', 'a', 's', 'e', 't', 'l'];
+let pangrams = ["basketball","bleakest"];
+let matchingwordsarray = [
+    "asks",
+    "bake",
+    "bakes",
+    "bask",
+    "basket",
+    "basketball",
+    "baskets",
+    "basks",
+    "beak",
+    "beaks",
+    "bleak",
+    "bleakest",
+    "elks",
+    "kale",
+    "kebab",
+    "kebabs",
+    "keel",
+    "keels",
+    "kelt",
+    "kelts",
+    "kettle",
+    "kettles",
+    "lake",
+    "lakes",
+    "leak",
+    "leaks",
+    "leek",
+    "leeks",
+    "sake",
+    "sakes",
+    "seek",
+    "seeks",
+    "skate",
+    "skates",
+    "skeletal",
+    "slake",
+    "sleek",
+    "sleeks",
+    "stake",
+    "stakes",
+    "stalk",
+    "stalks",
+    "steak",
+    "steaks",
+    "take",
+    "takeable",
+    "takes",
+    "talk",
+    "talks",
+    "task",
+    "tasks",
+    "teak"
+  ];
+
+router.get('/playaa', (req, res) => {
+    res.render('spelling2', {
+         pageTitle: 'Play local test Bee',
+         path: '/spelling2a',
+         word: word,
+         wordarray: remaininglettersarray,
+         centerletter: centreletter,
+         allmatchingwordsa: matchingwordsarray,
+         pangrams: pangrams
+    });       
+})
+
+
 router.get('/howmanypangrams', BeeController.howmanypangrams);
 
 module.exports = router;
